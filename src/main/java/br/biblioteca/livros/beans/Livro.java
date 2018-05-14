1f45c7ff3d5340df1c2ca3d031051606bef845c2
Daniel Corrêa <danccorrrea@gmail.com>
2018-05-14 02:03:34 -0300

    Spring Boot commit

---
+++ src/main/java/br/biblioteca/livros/beans/Livro.java
@@ -0,0 +1,69 @@
+package br.biblioteca.livros.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Livro {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@NotNull
+	@Size(min = 2, max = 100)
+	private String titulo;
+	
+	private String foto;
+	
+	@NotNull
+	private int quantidade;
+	
+	@ManyToOne
+	private Autor autor;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Autor getAutor() {
+		return autor;
+	}
+
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO AUTOR (NOME) VALUES ('Manoel Bandeira');
+INSERT INTO AUTOR (NOME) VALUES ('Gracilianos Ramos');
+
+INSERT INTO  LIVRO ( ID, TITULO, QUANTIDADE ) VALUES ( 1, 'Use a cabeça Java', 30 );
+INSERT INTO  LIVRO ( ID, TITULO, QUANTIDADE ) VALUES ( 2, 'Java como programar', 50 );
+INSERT INTO  LIVRO ( ID, TITULO, QUANTIDADE ) VALUES ( 3, 'Spring boot', 20 );
